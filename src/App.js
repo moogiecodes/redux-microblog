fc3c3573c48274b34093f2bc34518edb48f1faa1
Nicolas Guerrero <saynaysurf@gmail.com>
2020-03-12 12:14:59 -0700

    forgot to save App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import './App.css';
 import NavBar from './NavBar';
-import Routes from './Routes';
-import { Route, Switch } from 'react-router-dom';
+
+import { Route, Switch, Redirect } from 'react-router-dom';
+import TitleList from './TitleList';
+import NewPostContainer from './NewPostContainer';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Routes />
+      <Switch>
+        <Route exact path="/">
+          <TitleList/>
+        </Route>
+        <Route exact path="/new">
+          <NewPostContainer/>
+        </Route>
+        <Route exact path="/:postId">
+
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+
     </div>
   );
 }
