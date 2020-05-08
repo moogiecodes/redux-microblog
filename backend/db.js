46b823695274c91e8a539c41fa1f37b6d4931ce8
moogiecodes <mchoichoi@gmail.com>
2020-05-08 12:17:41 -0700

    ready to deploy backend

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,13 @@
 /** Database connection for Microblog. */
 
 const { Client } = require("pg");
+const SECRET = process.env.SECRET_KEY || 'test';
 
 const client = new Client(process.env.DATABASE_URL || "postgresql:///microblog");
 
 client.connect();
 
-
-module.exports = client;
+module.exports = {
+  client,
+  SECRET
+};
