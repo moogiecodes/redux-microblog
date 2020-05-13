18c6604aa1ac6ebbabf0115cf3f9269a61284462
moogiecodes <mchoichoi@gmail.com>
2020-05-13 15:56:59 -0700

    Change port

--- backend/config.js
+++ backend/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const SECRET = process.env.SECRET_KEY || 'test';
 
-const PORT = +process.env.PORT || 3001;
+const PORT = +process.env.PORT || 5000;
 
 console.log(PORT);
 
